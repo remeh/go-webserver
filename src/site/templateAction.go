deaa5123633705e0455ec705826728c23e2b500e
remeh <remeh@remeh.fr>
2014-04-18 13:28:54 +0200

    Fixed the example using template.

--- src/site/templateAction.go
+++ src/site/templateAction.go
@@ -20,14 +20,17 @@ type TemplateParams struct {
 func (a *TemplateAction) Init() {
     page, err := webserver.CreateDynamicPage("templatetest", &a.App.Router, "templates/test.htm");
     if (err != nil) {
-        a.page = page;
-    } else {
         panic(err);
+    } else {
+        a.page = page;
     }
 }
 
 func (a *TemplateAction) Execute(writer http.ResponseWriter, request *http.Request, parameters map[string]string) (int, string) {
-    result,_ := a.page.Render(TemplateParams{"Content to insert in the template."});
+    name := "No name given.";
+    if (parameters["name"] != "") {
+        name = parameters["name"];
+    }
+    result,_ := a.page.RenderNamedTemplate("test.htm", TemplateParams{name});
     return 200, result;
 }
-
