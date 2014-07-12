5cb6908c92c81a1d702c1978ba84dd4bb6612072
remeh <remeh@remeh.fr>
2014-07-12 10:29:41 +0200

    Moved everything into the root

---
+++ action.go
@@ -0,0 +1,13 @@
+package webserver
+
+import (
+	"net/http"
+)
+
+// ----------------------
+// Declarations
+
+type Action interface {
+	Init()
+	Execute(writer http.ResponseWriter, request *http.Request, parameters map[string]string) (int, string)
+}
--- simplestringwriter.go
+++ simplestringwriter.go
@@ -1,14 +1,14 @@
-package webserver;
+package webserver
 
 /**
  * A simple writer which writes into a string.
  * @author Rémy MATHIEU
  */
 type SimpleStringWriter struct {
-    Value string;
+	Value string
 }
 
 func (s *SimpleStringWriter) Write(p []byte) (int, error) {
-    s.Value += string(p);
-    return len(p), nil;
+	s.Value += string(p)
+	return len(p), nil
 }
